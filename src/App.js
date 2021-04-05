5500a43a902d8ef7256925923a82072a8d99936e
Blake DeGraw <bbdegraw@gmail.com>
2021-04-05 14:50:28 -0700

    signing off for the day

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import React, { Link, Route, BrowserRouter as Router } from 'react-router-dom'
-import React from 'react'
+import React, { useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import SwiperCore, { Navigation, Pagination } from 'swiper'
+import SwiperCore, { Navigation, Controller, Pagination, Thumbs } from 'swiper'
 import 'swiper/swiper-bundle.css'
 // import { useState } from 'react'
 import './styles.css'
@@ -10,11 +10,13 @@ import './styles.css'
 // import test3 from './components/test3'
 // import test4 from './components/test4'
 
-SwiperCore.use([Navigation, Pagination])
+SwiperCore.use([Navigation, Pagination, Controller, Thumbs])
 
 function App() {
-  const slides = []
+  const [thumbsSwiper, setThumbsSwiper] = useState()
+  const [controlledSwiper, setControlledSwiper] = useState()
 
+  const slides = []
   for(let i = 0; i < 5; i++){
     slides.push(
       <SwiperSlide key={`slide-${i}`} tag="li">
@@ -23,8 +25,39 @@ function App() {
     )
   }
 
+  const thumbs = []
+  for(let i = 0; i < 5; i++){
+    thumbs.push(
+      <SwiperSlide 
+        key={`thumb-${i}`} 
+        tag="li" 
+        style={{ listStyle: 'none' }}
+      >
+        <img 
+          src={`https://picsum.photos/id/${i}/163/100`} 
+          alt={`Thumbnail ${i}`}
+          ></img>
+      </SwiperSlide>
+    )
+  }
+
+  const slides2 = []
+  for(let i = 5; i < 10; i++){
+    slides2.push(
+      <SwiperSlide key={`slide-${i}`} tag="li">
+        <img 
+          src={`https://picsum.photos/id/${i+1}/500/300`}
+          style={{ listStyle: 'none' }}
+          alt={`Slide ${i}` }
+          />
+      </SwiperSlide>
+    )
+  }
+
   return <React.Fragment>
     <Swiper id="main" 
+            thumbs={{ swiper: thumbsSwiper }}
+            controller={{ control: controlledSwiper }}
             tag="section" 
             wrapperTag="ul" 
             navigation 
@@ -33,10 +66,23 @@ function App() {
             slidesPerView={1} 
             onInit={(swiper) => console.log('Swiper Initialized!')}
             onSlideChange={(swiper) => {console.log('Slide index changed to: ', swiper.activeIndex)}}
-            onReachEnd={() => console.log('Swiper end reached')}
-            >
+            onReachEnd={() => console.log('Swiper end reached')}>
       {slides}
     </Swiper>
+
+    <Swiper 
+      id="thumbs" 
+      spaceBetween={5} 
+      slidesPerView={3} 
+      onSwiper={setThumbsSwiper}
+      >
+      {thumbs}
+    </Swiper>
+
+  <Swiper id="controller" onSpwiper={setControlledSwiper}>
+    {slides2}
+  </Swiper>
+
   </React.Fragment>
   // return (
   //   <div>
