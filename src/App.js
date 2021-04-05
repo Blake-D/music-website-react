ce3f0fe0e6ef1a04000e4689b40972ad0a6f4052
Blake DeGraw <bbdegraw@gmail.com>
2021-04-05 11:58:42 -0700

    initial push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Link, Route, BrowserRouter as Router } from 'react-router-dom'
+import { useState } from 'react'
+import './App.css'
+import test1 from './components/test1'
+import test2 from './components/test2'
+import test3 from './components/test3'
+import test4 from './components/test4'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <Router>
+          <Link to="/test1">test1</Link>
+          <Link to="/test2">test2</Link>
+          <Link to="/test1">test3</Link>
+          <Link to="/test4">test4</Link>
+          <Route path="/test1" component={test1}/>
+          <Route path="/test2" component={test2}/>
+          <Route path="/test3" component={test3}/>
+          <Route path="/test4" component={test4}/>
+        </Router>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/test4.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function test4(){
+    return(
+        <div>
+            <h1>test4</h1>
+        </div>
+    )
+}
+
+export default test4
