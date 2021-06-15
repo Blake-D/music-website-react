e2d47ebc4ad44dad5744bd849ac8ebee58c4bac5
Blake DeGraw <bbdegraw@gmail.com>
2021-06-15 14:42:41 -0700

    got the basic foundations of parallax scrolling laid out

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react'
 import './styles.css'
-import Header from './components/header'
+import Pic from './images/home_pic.jpg'
+// import Header from './components/header'
 
 function App() {
   return (
     <div id="body">
-      <Header />
+      <div className="parallax">
+        <div className="parallax__layer parallax__layer--base">
+          <h1>kjsdhfljasdhflsuhDFLshdflkshDFLshdfljshdflsdlfjsldjfsljdf</h1><br></br>
+          <h1>kjsdhfljasdhflsuhDFLshdflkshDFLshdfljshdflsdlfjsldjfsljdf</h1><br></br>
+          <h1>kjsdhfljasdhflsuhDFLshdflkshDFLshdfljshdflsdlfjsldjfsljdf</h1><br></br>
+          <h1>kjsdhfljasdhflsuhDFLshdflkshDFLshdfljshdflsdlfjsldjfsljdf</h1><br></br>
+          <h1>kjsdhfljasdhflsuhDFLshdflkshDFLshdfljshdflsdlfjsldjfsljdf</h1><br></br>
+          <h1>kjsdhfljasdhflsuhDFLshdflkshDFLshdfljshdflsdlfjsldjfsljdf</h1><br></br>
+          <h1>kjsdhfljasdhflsuhDFLshdflkshDFLshdfljshdflsdlfjsldjfsljdf</h1><br></br>
+          <h1>kjsdhfljasdhflsuhDFLshdflkshDFLshdfljshdflsdlfjsldjfsljdf</h1><br></br>
+          <h1>kjsdhfljasdhflsuhDFLshdflkshDFLshdfljshdflsdlfjsldjfsljdf</h1><br></br>
+          <h1>kjsdhfljasdhflsuhDFLshdflkshDFLshdfljshdflsdlfjsldjfsljdf</h1><br></br>
+        </div>
+        <div className="parallax__layer parallax__layer--back"></div>
+      </div>
     </div>
+    // <div id="body">
+    //   <Header />
+    //   <div id="test-area">testing</div>
+    // </div>
   )
 }
 
