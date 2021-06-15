6155937fb97c9c2e5ff7a8eb6e90da3ccf145e7a
Blake DeGraw <bbdegraw@gmail.com>
2021-06-15 15:17:25 -0700

    parallax scrolling is generally working how I want it to, but I can't reposition the background image

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import './styles.css'
 import Pic from './images/home_pic.jpg'
-// import Header from './components/header'
+import Header from './components/header'
 
 function App() {
   return (
     <div className="parallax">
       <div className="parallax__group">
         <div className="parallax__layer parallax__layer--back">
-          back
+          
         </div>
         <div className="parallax__layer parallax__layer--base">
-          base
+          <Header />
         </div>
       </div>
       <div className="parallax__group">
