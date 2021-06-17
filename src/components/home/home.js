e3581762247b4dda759bb794160f35422455e67d
Blake DeGraw <bbdegraw@gmail.com>
2021-06-17 14:53:59 -0700

    added a song-of-the-week section to home page and redistributed the colors so the sections alternate

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from './header'
 import Blurb from './blurb'
 import Contact from './contact'
+import SongOfWeek from './songofweek'
 
 function Home() {
   return (
     <div id="home">
       <Header />
       <Blurb />
+      <SongOfWeek />
       <Contact />
     </div>
   )
---
+++ src/components/home/songofweek.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function SongOfWeek(){
+    return(
+        <div id="song-of-week">
+            <h1>Song of the Week</h1>
+            <p>here's a little blurb about this week's song</p>
+            <p>here's where the music player is embedded</p>
+        </div>
+    )
+}
+
+export default SongOfWeek
