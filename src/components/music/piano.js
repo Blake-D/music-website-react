2efdd4e010ca129f4fdb116fb565ed2683e8e7f4
Blake DeGraw <bbdegraw@gmail.com>
2021-06-21 12:48:13 -0700

    figured out how to play audio hosted on google drive. currently playing houndstooth in /music/piano

--- src/components/music/piano.js
+++ src/components/music/piano.js
@@ -2,10 +2,17 @@ import React from 'react'
 import Nav from '../nav'
 
 function Piano(){
+
+    // function Houndstooth() {
+    //     const audio = new Audio(Pickle)
+    //     audio.play()
+    // }
+
     return(
         <div>
             <Nav />
             <h1>Piano music here</h1>
+            <audio src="https://drive.google.com/uc?export=download&id=1gGGm_6rsGhnc2nTeoZn6QhWjb6drC95Z" controls autoPlay/>
         </div>
     )
 }
